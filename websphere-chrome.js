a8354b3d7f3ae1bea6b265796da9612a55883ff7
Aviram Segal <aviram@aviramsegal.com>
2012-02-26 09:32:47 +0200

    websphere-chrome initial

---
+++ websphere-chrome.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name       WebSphere Change Log Detail Levels Fixer
+// @namespace  http://aviramsegal.com/websphere/log/fixer
+// @version    0.1
+// @description  Enabled +/- in the Change Log Detail Levels Page
+// @include    https://*/loggingSettingsComponents.jsp
+// @copyright  2011+, Aviram segal
+// ==/UserScript==
+
+var elements = document.getElementsByTagName('*');
+for (var i = elements.length - 1; i >= 0; i--)
+{
+    var elm = elements[i];
+    var name = elm.getAttribute('name');
+    if (name)
+    {
+        elm.setAttribute('id', name);
+    }
+}
